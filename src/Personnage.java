740123a6b0c4d459a128221d6e9b31617ba68c54
Tiity <anthony.cadet.94200@gmail.com>
2016-02-29 09:27:27 +0100

    Class Personnage

---
+++ src/Personnage.java
@@ -0,0 +1,83 @@
+/**
+ * Created by Anthony on 29/02/2016.
+ */
+public abstract class Personnage implements Attaque {
+
+    private Integer hp;
+    private Integer def;
+    private Integer power;
+    private Integer force;
+    private Integer intelligence;
+    private Integer lvl;
+    private Integer xp;
+
+    public Personnage() {
+
+        setDef(null);
+        setHp(null);
+        setForce(null);
+        setIntelligence(null);
+        setLvl(1);
+        setPower(null);
+        setXp(null);
+    }
+
+    // GETTERS
+    public Integer getDef() {
+        return def;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public Integer getForce() {
+        return force;
+    }
+
+    public Integer getIntelligence() {
+        return intelligence;
+    }
+
+    public Integer getLvl() {
+        return lvl;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public Integer getXp() {
+        return xp;
+    }
+
+    // SETTERS
+    public void setDef(Integer def) {
+        this.def = def;
+    }
+
+    public void setForce(Integer force) {
+        this.force = force;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
+    }
+
+    public void setIntelligence(Integer intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public void setLvl(Integer lvl) {
+        this.lvl = lvl;
+    }
+
+    public void setXp(Integer xp) {
+        this.xp = xp;
+    }
+
+}
