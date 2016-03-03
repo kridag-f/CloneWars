1163a6aa7333781fac8b126d34b64b3c8baf29f4
kridag-f <kridag_f@etna-alternance.net>
2016-03-03 15:39:13 +0100

    ajout recommencer + correction de la gestion d'erreur

--- src/Jeu.java
+++ src/Jeu.java
@@ -18,7 +18,7 @@ public class Jeu {
     private Integer choixAttaque;
     private Hashtable<Integer, Personnage> tabChoixClasse;
     private Personnage leJoueur;
-    private Integer voirStats;
+    private Integer recommencer;
 
     public Jeu() {
 
@@ -27,19 +27,16 @@ public class Jeu {
         setNomJoueur(null);
         setChoixEnnemi(0);
         setChoixAttaque(0);
-        setVoirStats(0);
+        setRecommencer(0);
         setChoixJoueur(null);
         setTabChoixClasse(new Hashtable<Integer, Personnage>());
         setLeJoueur(null);
+
     }
 
     public void start() {
 
-        menuStart(true);
-
-        while (getChoixJoueur() == null){
-            menuStart(false);
-        }
+        menuStart();
 
         tabChoix();
 
@@ -74,7 +71,7 @@ public class Jeu {
                     setChoixEnnemi(choixEnnemyAttaquer);
                     checkNumber = true;
                 } catch (InputMismatchException e) {
-                    System.out.println("\nEntrez un nombre entre un 1 et 4\n");
+                    System.out.println("\nVous devez entrer un nombre entre un 1 et 4\n");
                     getEntreStd().next();
                     checkNumber = false;
                 }
@@ -104,7 +101,7 @@ public class Jeu {
                             setChoixAttaque(getEntreStd().nextInt());
                             checkNumber = true;
                         } catch (InputMismatchException e) {
-                            System.out.println("Entrez un nombre entre un 1 et 5\n");
+                            System.out.println("Vous devez entrer un nombre entre un 1 et 5\n");
                             getEntreStd().next();
                             checkNumber = false;
                         }
@@ -124,10 +121,53 @@ public class Jeu {
                     }
 
                     if (leJoueur.getHp() <= 0) {
-                        System.out.println("Votre " + leJoueur.getClass().getName() + " est mort !!!\n");
-                        System.out.println("GAME OVER");
-                        System.exit(0);
-                        setGameOver(true);
+                        System.out.println("\nVotre " + leJoueur.getClass().getName() + " est mort !!!\n");
+                        System.out.println("GAME OVER\n");
+                        checkNumber = false;
+
+                        do {
+                            if (checkNumber) System.out.println("Entrez 0 ou 1 !!\n");
+
+                            System.out.println("Tapez 0 pour quittez");
+                            System.out.println("Tapez 1 pour recommencer");
+                            System.out.print("Votre choix : ");
+
+
+                            try {
+                                setRecommencer(getEntreStd().nextInt());
+                                System.out.println("");
+                                checkNumber = true;
+                            } catch (InputMismatchException e) {
+                                System.out.println("Vous devez entrer 0 ou 1 !!\n");
+                                getEntreStd().next();
+                                checkNumber = false;
+                            }
+                            if (getChoixAttaque() == 4) getStat(leJoueur);
+                            else if (getChoixAttaque() == 5) getStat(ennemie);
+
+                        } while (getRecommencer() < 0 || getRecommencer() > 1);
+
+
+
+                        if (getRecommencer() == 1) {
+                            setEntreStd(new Scanner(System.in));
+                            setGameOver(false);
+                            setNomJoueur(null);
+                            setChoixEnnemi(0);
+                            setChoixAttaque(0);
+                            setRecommencer(0);
+                            setChoixJoueur(null);
+                            setTabChoixClasse(new Hashtable<Integer, Personnage>());
+                            setLeJoueur(null);
+                            factory = null;
+                            usine = null;
+                            room = null;
+                            start();
+                        } else {
+                            setGameOver(true);
+                            System.exit(0);
+                        }
+
                     }
 
                 }
@@ -161,29 +201,36 @@ public class Jeu {
         System.out.println("XP : " + perso.getXp() + "\n");
     }
 
-    public void menuStart(Boolean first) {
+    public void menuStart() {
 
-        if (first) {
-            System.out.print("Donnez un nom à votre personnage : ");
-            setNomJoueur(getEntreStd().nextLine());
-        }
+        System.out.print("Donnez un nom à votre personnage : ");
+        setNomJoueur(getEntreStd().nextLine());
 
-        System.out.println("\nTapez 1 pour un Jedi");
-        System.out.println("Tapez 2 pour un Sith");
-        System.out.println("Tapez 3 pour un BountyHunters");
-        System.out.println("Tapez 4 pour un ImperialAgent");
-        System.out.println("Tapez 5 pour un Mercenary");
-        System.out.println("Tapez 6 pour un Jawa");
-        System.out.print("Votre choix : ");
+        Boolean checkNumber = false;
 
-        try {
-            Integer choixFin = getEntreStd().nextInt();
-            setChoixJoueur(choixFin);
-        } catch (InputMismatchException e) {
-            System.out.println("\nEntrez un nombre entre un 1 et 6\n");
-            getEntreStd().next();
-        }
+        do {
+            if (checkNumber) System.out.println("\nEntrez un nombre entre un 1 et 6");
 
+
+            System.out.println("\nTapez 1 pour un Jedi");
+            System.out.println("Tapez 2 pour un Sith");
+            System.out.println("Tapez 3 pour un BountyHunters");
+            System.out.println("Tapez 4 pour un ImperialAgent");
+            System.out.println("Tapez 5 pour un Mercenary");
+            System.out.println("Tapez 6 pour un Jawa");
+            System.out.print("Votre choix : ");
+
+            try {
+                Integer choixFin = getEntreStd().nextInt();
+                setChoixJoueur(choixFin);
+                checkNumber = true;
+            } catch (InputMismatchException e) {
+                System.out.println("\nVous devez entrer un nombre entre un 1 et 6");
+                setChoixJoueur(0);
+                getEntreStd().next();
+                checkNumber = false;
+            }
+        } while (getChoixJoueur() < 1 || getChoixJoueur() > 6);
 
     }
 
@@ -218,8 +265,8 @@ public class Jeu {
         return choixAttaque;
     }
 
-    public Integer getVoirStats() {
-        return voirStats;
+    public Integer getRecommencer() {
+        return recommencer;
     }
 
     public String getNomJoueur() {
@@ -255,8 +302,8 @@ public class Jeu {
         this.choixAttaque = choixAttaque;
     }
 
-    public void setVoirStats(Integer voirStats) {
-        this.voirStats = voirStats;
+    public void setRecommencer(Integer recommencer) {
+        this.recommencer = recommencer;
     }
 
     public void setNomJoueur(String nomJoueur) {
